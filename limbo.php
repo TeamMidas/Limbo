c965ac239e6d9d21677def4a300e5d15aaadf0aa
Antony Liang <findaliang@gmail.com>
2013-11-13 01:04:16 -0500

    added some styling to landing page; moved files out of random stuff

--- limbo.php
+++ limbo.php
@@ -12,17 +12,17 @@ $query = 'SELECT create_date, status, name FROM stuff ORDER BY create_date DESC'
 $results = mysqli_query($dbc, $query) ;
 
 
-echo "<a href='./lost.php'>Lost Something</a>" ;
-echo "<a href='./found.php'>Found Something</a>" ;
+echo "<a href='./lost.php' style='margin-right:10px'>Lost Something</a>" ;
+echo "<a href='./found.php' style='margin-right:10px'>Found Something</a>" ;
 echo "<a href='./admin.php'>Admins</a>" ;
 echo "<h1>Welcome to Limbo!</h1>" ;
 echo "<p>If you lost or found something, you're in luck: this is the place to report it." ;
-echo "<h4>Reported in last</h4>" ;
+echo "<h4 style='display:inline'>Reported in last </h4>" ;
 #dropdown menu
-echo "<select>" ;
-echo "<option value='7'>7 days</option>" ;
-echo "<option value='30'>30 days</option>" ;
-echo "<option value='365'>1 year</option>" ;
+echo "<select style='margin-bottom:10px'>" ;
+echo "<option value='week'>7 days</option>" ;
+echo "<option value='month'>1 month</option>" ;
+echo "<option value='trimonth'>3 months</option>" ;
 echo "</select>" ;
 
 if( $results )
